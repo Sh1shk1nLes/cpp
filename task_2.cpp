e19dca55201114c4f6cfb5f7c465f821d6963dcb
Sh1shk1nLes <128575123+Sh1shk1nLes@users.noreply.github.com>
2023-10-25 18:27:22 +0300

    Add files via upload

---
+++ task_2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class StackArr {
+private:
+    int* data;
+    int capacity;
+    int size;
+
+public:
+
+    StackArr(int capacity) {
+        this->capacity = capacity;
+        this->data = new int[capacity];
+        this->size = 0;
+    }
+
+    ~StackArr() {
+        delete[] data;
+    }
+
+    void push(int value) {
+        if (!isFull()) {
+            data[size] = value;
+            size++;
+        }
+        else {
+            std::overflow_error("—тек переполнен");
+        }
+    }
+
+    void pop() {
+        if (!isEmpty()) size--;
+    }
+
+    void display() {
+        for (int i = 0; i < size; i++) cout << data[size - i - 1] << " ";
+        cout << endl;
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size == capacity;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    
+    StackArr myStack(5);
+
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.push(5);
+    myStack.push(6); //проверка выхода за размерность
+
+    myStack.display();
+
+    myStack.pop();
+    myStack.pop();
+    myStack.pop();
+
+    myStack.display();
+
+
+    myStack.pop();
+    myStack.pop();
+
+    myStack.display();
+
+    myStack.pop(); //проверка удалени€ элемента пустого стека
+
+    myStack.display();
+}
